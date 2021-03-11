1d6be70ec729b6d0fe21fd56e579d446fc1fd566
Erick Yamato <erick.yamato@me.com>
2021-03-11 11:17:59 -0300

    added alamofire dependency

---
+++ PunkBeer/PunkBeer/Network/Requests/BeersListRequest.swift
@@ -0,0 +1,10 @@
+//
+//  BeersListRequest.swift
+//  PunkBeer
+//
+//  Created by Erick Mitsugui Yamato on 11/03/21.
+//  Copyright © 2021 Erick Yamato. All rights reserved.
+//
+
+import Foundation
+
